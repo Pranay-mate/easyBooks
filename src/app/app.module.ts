52a27696293b75dfa91eed41b18e52fcd32d4690
Shraddha Bijotkar <shraddha.bijotkar@gmail.com>
2020-08-04 22:46:40 +0530

    Shraddha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatVideoModule } from 'mat-video';
 import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +47,14 @@ import { FooterComponent } from './footer/footer.component';
     BrowserAnimationsModule,
     MatVideoModule,
     MatCardModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatDividerModule,
+    MatTabsModule,
     NgbModule
   ],
   providers: [],
